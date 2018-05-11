48169a37d59c6a8d2f11c39bfb1baaf7e15b5bd3
Jeremy LeCroy <jeremy212@gmail.com>
2018-05-11 18:18:49 -0400

    fixed python bug

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def hello_world():
 	return  ret_str
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ("PORT"))
+    app.run(host='0.0.0.0', port=os.environ.get("PORT",8080))
